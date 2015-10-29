a811f2a007e8bd81d8533d4860622068a7dd0766
Robert Machmer <robert.machmer@gmail.com>
2015-10-29 14:36:41 +0100

    Improve Lesson 4 Exercise 5
    Instead of starting at the bottom we start at the highest month and
    simply add each month to the previous case (note the removed break
    statements).

--- Lesson 4/Exercise5.java
+++ Lesson 4/Exercise5.java
@@ -12,43 +12,32 @@ public class Exercise5 {
         // Calculate the amount of days we have for each month.
         // Start at month - 1 because we have to ignore the month
         // we are currently in.
+        // NOTE: Linters might give you a warning for possible fall-throughs because of the missing breaks.
         switch(month - 1) {
-            case 0:
-                totalDays = 0;
-                break;
-            case 1:
-                totalDays = 31;
-                break;
-            case 2:
-                totalDays = 31 + 28;
-                break;
-            case 3:
-                totalDays = 31 + 28 + 31;
-                break;
-            case 4:
-                totalDays = 31 + 28 + 31 + 30;
-                break;
-            case 5:
-                totalDays = 31 + 28 + 31 + 30 + 31;
-                break;
-            case 6:
-                totalDays = 31 + 28 + 31 + 30 + 31 + 30;
-                break;
-            case 7:
-                totalDays = 31 + 28 + 31 + 30 + 31 + 30 + 31;
-                break;
-            case 8:
-                totalDays = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
-                break;
-            case 9:
-                totalDays = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
-                break;
-            case 10:
-                totalDays = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
-                break;
             case 11:
-                totalDays = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
-                break;
+                totalDays += 30;
+            case 10:
+                totalDays += 31;
+            case 9:
+                totalDays += 30;
+            case 8:
+                totalDays += 31;
+            case 7:
+                totalDays += 31;
+            case 6:
+                totalDays += 30;
+            case 5:
+                totalDays += 31;
+            case 4:
+                totalDays += 30;
+            case 3:
+                totalDays += 31;
+            case 2:
+                totalDays += 28;
+            case 1:
+                totalDays += 31;
+            case 0:
+                totalDays += 0; // Superfluous but left here for completeness.
         }
 
         // Add the day we are currently on in the month.
