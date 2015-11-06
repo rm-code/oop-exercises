c809d0b1834eed5e4e3b14231037da280dc5145e
Robert Machmer <robert.machmer@gmail.com>
2015-11-06 11:07:55 +0100

    Add Lesson 5 Exercise 2

---
+++ Lesson 5/Exercise2.java
@@ -0,0 +1,20 @@
+public class Exercise2 {
+    static double calculateFaculty(int number) {
+        double fac = 1;
+
+        for (int i = 1; i <= number; i++) {
+            fac *= i;
+        }
+
+        return fac;
+    }
+
+    public static void main(String[] args) {
+        final int NUMBER = 5;
+
+        // Calculate faculty.
+        double result = calculateFaculty(NUMBER);
+
+        System.out.println("The faculty of " + NUMBER + " is " + result);
+    }
+}
