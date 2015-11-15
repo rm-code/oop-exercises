a8bc3e136b3a0f86b582b12aea42c4be0b669fd7
Robert Machmer <robert.machmer@gmail.com>
2015-11-15 15:39:18 +0100

    Add pretty indentation to the pascal's triangle

--- Lesson 6/Exercise4.java
+++ Lesson 6/Exercise4.java
@@ -5,11 +5,11 @@ public class Exercise4 {
      * position we simply use zero as a substitute.
     **/
     public static void main(String[] args) {
-        final int depth = 10;
+        final int depth = 15;
         int[] line = {}; // This array is used to create each line of the triangle.
 
         // Repeat until the desired depth for the triangle is reached.
-        for (int i = 0; i < depth; i++) {
+        for (int i = 1; i <= depth; i++) {
              // Create a new array for each line which is one index longer than the previous one.
             int[] tmp = new int[line.length + 1];
 
@@ -20,9 +20,26 @@ public class Exercise4 {
                 tmp[j] = (a + b == 0) ? 1 : a + b;
             }
 
+            // This adds whitespace in front of each line to create the triangle.
+            // NOTE: This was not part of the actual exercise.
+            for (int j = 1; j <= depth - i; j++) {
+                System.out.print("   ");
+            }
+
             // Print line to the console.
             for (int j = 0; j < tmp.length; j++) {
-                System.out.printf("%4d", tmp[j]);
+                // Match the whitespace based on the value of the element.
+                // NOTE: This whole block could be replaced easily with printf:
+                // System.out.printf("%6d", tmp[j]);
+                if (tmp[j] < 10) {
+                    System.out.print("     " + tmp[j]);
+                } else if(tmp[j] < 100) {
+                    System.out.print("    " + tmp[j]);
+                } else if(tmp[j] < 1000) {
+                    System.out.print("   " + tmp[j]);
+                } else {
+                    System.out.print("  " + tmp[j]);
+                }
             }
             System.out.println("");
 
