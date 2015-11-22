db440dc6daa4f41715304fa56e84b2286a31bd7d
Robert Machmer <robert.machmer@gmail.com>
2015-11-22 19:50:19 +0100

    Add Lesson 7 Exercise 6

---
+++ Lesson 7/Exercise 6/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main( String[] args ) {
+        Scanner input = new Scanner(System.in);
+        TrafficLight light = new TrafficLight();
+
+        while ( true ) {
+            String line = input.nextLine();
+            if (line.length() == 0) {
+                light.change();
+                System.out.println( light.getColor() );
+            }
+        }
+    }
+}
