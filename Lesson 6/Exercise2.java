8f9925fd77dd7b5372e07c256c508de8450a114f
Robert Machmer <robert.machmer@gmail.com>
2015-11-22 19:32:46 +0100

    Initialize maximum temperature at -272

--- Lesson 6/Exercise2.java
+++ Lesson 6/Exercise2.java
@@ -5,7 +5,7 @@ public class Exercise2 {
         System.out.println("Please enter the temperatures for this week: ");
 
         Scanner input = new Scanner(System.in);
-        int max   = 0; // Stores our maximum temperature.
+        int max   = -272; // Stores our maximum temperature.
         int total = 0; // Stores the total sum of temperatures.
 
         for(int i = 0; i < 7; i++) {
