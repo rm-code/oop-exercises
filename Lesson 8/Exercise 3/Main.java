91a0bb715e045e7f855ca6e0c72bf4f164c5ffc7
Robert Machmer <robert.machmer@gmail.com>
2015-11-30 22:09:01 +0100

    Solve Lesson 8 Exercise 3 b)

--- Lesson 8/Exercise 3/Main.java
+++ Lesson 8/Exercise 3/Main.java
@@ -4,8 +4,14 @@ public class Main {
         Rectangle rectangle = new Rectangle( "Rectangle", "blue", 3, 10 );
         Square square = new Square( "Square", "green", 5 );
 
-        System.out.println(circle);
-        System.out.println(rectangle);
-        System.out.println(square);
+        GeometryObject[] objects = {
+            new Circle( "Circle", "red", Math.random() * 5 ),
+            new Rectangle( "Rectangle", "blue", Math.random() * 10, Math.random() * 10 ),
+            new Square( "Square", "green", Math.random() * 5 ),
+        };
+
+        for (int i = 0; i < objects.length; i++) {
+            System.out.println( objects[i].toString() );
+        }
     }
 }
