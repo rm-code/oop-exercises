9dd43ebec4c8a3daca3f22d8516329672c90e380
Robert Machmer <robert.machmer@gmail.com>
2016-02-01 23:37:11 +0100

    Shorten code

--- Lesson 5/Exercise5.java
+++ Lesson 5/Exercise5.java
@@ -1,7 +1,5 @@
 public class Exercise5 {
     public static void main(String[] args) {
-        double result = Math.random();
-        int roll = (int) (result * 6);
-        System.out.println("The die shows " + (roll + 1));
+        System.out.println("The die shows " + ( (int) ( Math.random() * 6 ) + 1 ));
     }
 }
