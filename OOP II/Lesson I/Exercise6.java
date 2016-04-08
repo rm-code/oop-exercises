51ce1a4cdb21a0f11bcef795041c269f0be84023
Robert Machmer <robert.machmer@gmail.com>
2016-04-08 22:36:10 +0200

    Add Lesson I Exercise 6

---
+++ OOP II/Lesson I/Exercise6.java
@@ -0,0 +1,71 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Modality;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+
+public class Exercise6 extends Application {
+
+    private void createNewWindow( Stage primary ) {
+        Stage secondary = new Stage( StageStyle.UTILITY );
+        secondary.setAlwaysOnTop( true );
+        secondary.initOwner( primary );
+        secondary.initModality( Modality.WINDOW_MODAL );
+
+        Button buttonOk = new Button( "Ok" );
+        buttonOk.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle( ActionEvent event ) {
+                Platform.exit();
+            }
+        });
+
+        Button buttonCancel = new Button( "Cancel" );
+        buttonCancel.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle( ActionEvent event ) {
+                secondary.close();
+            }
+        });
+
+        HBox root = new HBox();
+        root.getChildren().addAll(
+            buttonOk, buttonCancel
+        );
+
+        Scene scene = new Scene( root );
+        secondary.setScene( scene );
+        secondary.show();
+        secondary.centerOnScreen();
+    }
+
+    @Override
+    public void start( Stage primaryStage ) {
+        Button btn = new Button( "Exit" );
+        btn.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle( ActionEvent event ) {
+                createNewWindow( primaryStage );
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add( btn );
+
+        Scene scene = new Scene( root, 300, 250 );
+
+        primaryStage.setTitle( "Exercise 5" );
+        primaryStage.setScene( scene );
+        primaryStage.show();
+    }
+
+    public static void main( String[] args ) {
+        launch( args );
+    }
+}
