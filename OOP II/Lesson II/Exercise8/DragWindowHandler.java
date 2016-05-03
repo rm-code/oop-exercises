97904f7c1ed6bb89dfb029a80af1ca292a60d040
Robert Machmer <robert.machmer@gmail.com>
2016-05-03 09:33:34 +0200

    Improve mouse dragging

--- OOP II/Lesson II/Exercise8/DragWindowHandler.java
+++ OOP II/Lesson II/Exercise8/DragWindowHandler.java
@@ -7,10 +7,7 @@ import javafx.stage.Stage;
 public class DragWindowHandler implements EventHandler<MouseEvent> {
     Stage stage;
 
-    private double xOffset;
-    private double yOffset;
-    private double mouseX;
-    private double mouseY;
+    double xOffset, yOffset, mouseX, mouseY;
 
     public DragWindowHandler( Stage stage ) {
         this.stage = stage;
@@ -18,15 +15,16 @@ public class DragWindowHandler implements EventHandler<MouseEvent> {
 
     @Override
     public void handle( MouseEvent event ) {
+
         if ( event.isAltDown() ) {
             if ( event.getEventType() == MouseEvent.MOUSE_PRESSED ) {
-                xOffset = stage.getX() - event.getScreenX();
-                yOffset = stage.getY() - event.getScreenY();
-                mouseX = event.getSceneX();
-                mouseY = event.getSceneY();
+                xOffset = stage.getX();
+                yOffset = stage.getY();
+                mouseX = event.getScreenX();
+                mouseY = event.getScreenY();
             } else if ( event.getEventType() == MouseEvent.MOUSE_DRAGGED ) {
-                stage.setX( event.getScreenX() - mouseX + xOffset );
-                stage.setY( event.getScreenY() - mouseY + yOffset );
+                stage.setX( xOffset + ( event.getScreenX() - mouseX ));
+                stage.setY( yOffset + ( event.getScreenY() - mouseY ));
             }
         }
     }
