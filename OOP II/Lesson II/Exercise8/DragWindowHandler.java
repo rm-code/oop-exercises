770802f6ac4e8799986a99f4da9df4a67faeddc7
Robert Machmer <robert.machmer@gmail.com>
2016-05-03 10:18:44 +0200

    Refactor DragWindowHandler.java

--- OOP II/Lesson II/Exercise8/DragWindowHandler.java
+++ OOP II/Lesson II/Exercise8/DragWindowHandler.java
@@ -7,7 +7,8 @@ import javafx.stage.Stage;
 public class DragWindowHandler implements EventHandler<MouseEvent> {
     Stage stage;
 
-    double xOffset, yOffset, mouseX, mouseY;
+    double stageOriginX, stageOriginY; // The coordinates of the stage registered when the mouse is pressed.
+    double absoluteX, absoluteY;       // The absolute screen coordinates of the mouse click.
 
     public DragWindowHandler( Stage stage ) {
         this.stage = stage;
@@ -18,13 +19,17 @@ public class DragWindowHandler implements EventHandler<MouseEvent> {
 
         if ( event.isAltDown() ) {
             if ( event.getEventType() == MouseEvent.MOUSE_PRESSED ) {
-                xOffset = stage.getX();
-                yOffset = stage.getY();
-                mouseX = event.getScreenX();
-                mouseY = event.getScreenY();
+                stageOriginX = stage.getX();
+                stageOriginY = stage.getY();
+                absoluteX = event.getScreenX();
+                absoluteY = event.getScreenY();
             } else if ( event.getEventType() == MouseEvent.MOUSE_DRAGGED ) {
-                stage.setX( xOffset + ( event.getScreenX() - mouseX ));
-                stage.setY( yOffset + ( event.getScreenY() - mouseY ));
+                // Calculate the translation vector by subtracting the coordinates of the
+                // mouse press from the current mouse position. This vector can then be
+                // applied to the original stage position to move the stage to the correct
+                // position on screen.
+                stage.setX( stageOriginX + ( event.getScreenX() - absoluteX ));
+                stage.setY( stageOriginY + ( event.getScreenY() - absoluteY ));
             }
         }
     }
