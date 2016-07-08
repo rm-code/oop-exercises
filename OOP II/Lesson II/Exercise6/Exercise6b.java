277a001aba1afe0ebb56147cedc9449be8f48c14
Robert Machmer <robert.machmer@gmail.com>
2016-07-08 22:45:31 +0200

    Update Exercise6b.java

--- OOP II/Lesson II/Exercise6/Exercise6b.java
+++ OOP II/Lesson II/Exercise6/Exercise6b.java
@@ -2,83 +2,85 @@ package Exercise6;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Modality;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Text;
 
+public class Exercise6 extends Application {
 
-public class Exercise6b extends Application {
+    private void createNewWindow( Stage primary, BooleanProperty close ) {
+        Stage secondary = new Stage( StageStyle.UTILITY );
+        secondary.setAlwaysOnTop( true );
+        secondary.initOwner( primary );
+        secondary.initModality( Modality.WINDOW_MODAL );
+
+        Button buttonOk = new Button( "Ok" );
+        buttonOk.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle( ActionEvent event ) {
+                close.set( true );
+                secondary.close();
+            }
+        });
+
+        Button buttonCancel = new Button( "Cancel" );
+        buttonCancel.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle( ActionEvent event ) {
+                secondary.close();
+            }
+        });
+
+        HBox root = new HBox();
+        root.getChildren().addAll(
+                buttonOk, buttonCancel
+        );
+
+        Scene scene = new Scene( root );
+        secondary.setScene( scene );
+        secondary.showAndWait();
+        secondary.centerOnScreen();
+    }
 
     @Override
     public void start( Stage primaryStage ) {
-        Button btn = new Button( "Programm beenden" );
+        SimpleBooleanProperty close = new SimpleBooleanProperty( false );
 
-        BorderPane root = new BorderPane();
-        root.setCenter( btn );
-
-        Scene scene = new Scene( root, 400, 400 );
-        primaryStage.setTitle( "Aufgabe 6" );
-        primaryStage.setScene( scene );
-        primaryStage.show();
-
+        Button btn = new Button( "Exit" );
         btn.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
             public void handle( ActionEvent event ) {
-                SimpleBooleanProperty close = new SimpleBooleanProperty( false );
-
-                Button ok = new Button( "OK" );
-                Button cancel = new Button( "Abbruch" );
-                Text text = new Text( "Wollen Sie das Programm wirklich beenden?" );
-
-                Stage secondstage = new Stage();
-                secondstage.setTitle( "Information" );
-
-                BorderPane root2 = new BorderPane();
-                root2.setLeft( ok );
-                root2.setTop( text );
-                root2.setRight( cancel );
-
-                BorderPane.setAlignment( ok, Pos.BOTTOM_LEFT );
-                BorderPane.setAlignment( text, Pos.TOP_CENTER );
-                BorderPane.setAlignment( cancel, Pos.BOTTOM_RIGHT );
-
-                Scene scene2 = new Scene( root2, 350, 50 );
-                secondstage.setScene( scene2 );
-
-                secondstage.initModality( Modality.APPLICATION_MODAL );
-                secondstage.initOwner( primaryStage );
-
-                secondstage.show();
-
-                close.addListener( new ChangeListener<Object>(){
-                    public void changed( ObservableValue<?> observable, Object oldValue, Object newValue ){
-                        if ( close.get() == true ) {
-                            Platform.exit();
-                        }
-                    }
-                });
-
-                ok.setOnAction( new EventHandler<ActionEvent>() {
-                    public void handle( ActionEvent event ){
-                        close.set( true );
-                    }
-                });
-
-                cancel.setOnAction( new EventHandler<ActionEvent>() {
-                    public void handle( ActionEvent event ){
-                        secondstage.close();
-                    }
-                });
+                createNewWindow( primaryStage, close );
             }
         });
+
+        close.addListener( new ChangeListener<Boolean>() {
+            @Override
+            public void changed( ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue ) {
+                if ( newValue == true ) {
+                    Platform.exit();
+                }
+            }
+        } );
+
+        StackPane root = new StackPane();
+        root.getChildren().add( btn );
+
+        Scene scene = new Scene( root, 300, 250 );
+
+        primaryStage.setTitle( "Exercise 6" );
+        primaryStage.setScene( scene );
+        primaryStage.show();
     }
 
     public static void main( String[] args ) {
